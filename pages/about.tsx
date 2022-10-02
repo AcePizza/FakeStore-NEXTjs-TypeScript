3ea56b6a51d0c9e0b8df16c75ed58faf4e0e0504
AcePizza <stianchrbakke@gmail.com>
2022-10-02 16:02:16 +0200

    refactoring

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 type Props = {
@@ -18,16 +18,39 @@ type Data = {
   };
 };
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (props: Props) => {
   const res = await fetch("https://fakestoreapi.com/products");
   const data: Data = await res.json();
   return { props: { data } };
 };
 
 const about = (props: Props) => {
+  const [textValue, setTextValue] = useState<string>();
+
+  const onClickHandler = async () => {
+    console.log("This runs");
+    const res = await fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username: "mor_2314",
+        password: "83r5^_",
+      }),
+    });
+    const data = await res.json();
+    console.log("data :>> ", data);
+  };
+
+  const onChangeHandler = (e: { target: { value: string } }) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div>
       <h3 className={styles.title}>My About Page</h3>
+      <form>
+        <input value={textValue} onChange={onChangeHandler}></input>
+      </form>
+      <button onClick={onClickHandler}>Button</button>
       {props.data.map((element: Data, index: number) => {
         return (
           <React.Fragment key={index}>
